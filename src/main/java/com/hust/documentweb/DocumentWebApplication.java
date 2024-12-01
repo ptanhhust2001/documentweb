c097cd4203ace1abef7afac2c13fe382cf5b16c5
Phạm Tuấn Anh <anhpt.hust2001@gmail.com>
2024-12-01 10:00:49 +0700

    second commit

--- src/main/java/com/hust/documentweb/DocumentWebApplication.java
+++ src/main/java/com/hust/documentweb/DocumentWebApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IdentityServiceApplication {
+public class DocumentWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(IdentityServiceApplication.class, args);
+        SpringApplication.run(DocumentWebApplication.class, args);
     }
 }
